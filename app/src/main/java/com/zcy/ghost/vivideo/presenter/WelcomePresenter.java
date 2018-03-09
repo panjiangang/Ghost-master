594e6d4b508b5b3b0ac7f8c9ee9a8c01a4592f40
402792989 <402792989@qq.com>
2018-03-09 19:51:25 +0800

    Initial commit

---
+++ app/src/main/java/com/zcy/ghost/vivideo/presenter/WelcomePresenter.java
@@ -0,0 +1,62 @@
+package com.zcy.ghost.vivideo.presenter;
+
+import com.zcy.ghost.vivideo.base.RxPresenter;
+import com.zcy.ghost.vivideo.presenter.contract.WelcomeContract;
+import com.zcy.ghost.vivideo.utils.RxUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.functions.Action1;
+
+/**
+ * Description: WelcomePresenter
+ * Creator: yxc
+ * date: 2016/9/22 13:17
+ */
+public class WelcomePresenter extends RxPresenter<WelcomeContract.View> implements WelcomeContract.Presenter {
+
+    private static final int COUNT_DOWN_TIME = 2200;
+
+    @Inject
+    public WelcomePresenter() {
+    }
+
+    @Override
+    public void getWelcomeData() {
+
+        mView.showContent(getImgData());
+        startCountDown();
+    }
+
+    private void startCountDown() {
+        Subscription rxSubscription = Observable.timer(COUNT_DOWN_TIME, TimeUnit.MILLISECONDS)
+                .compose(RxUtil.<Long>rxSchedulerHelper())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                        mView.jumpToMain();
+                    }
+                });
+        addSubscribe(rxSubscription);
+    }
+
+    private List<String> getImgData() {
+        List<String> imgs = new ArrayList<>();
+        imgs.add("file:///android_asset/a.jpg");
+        imgs.add("file:///android_asset/b.jpg");
+        imgs.add("file:///android_asset/c.jpg");
+//        imgs.add("file:///android_asset/d.jpg");
+        imgs.add("file:///android_asset/e.jpg");
+
+        imgs.add("file:///android_asset/f.jpg");
+        imgs.add("file:///android_asset/g.jpg");
+        return imgs;
+    }
+
+}
